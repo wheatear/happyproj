89f58c959919ca1fe668704cff88a453e2c7e3f1
Wang Xintian <wxintian@163.com>
2018-10-31 20:21:03 +0800

    dev

--- dictation/admin.py
+++ dictation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from dictation.models import Press,Book,Unit,Lesson,Word
 
 # Register your models here.
+admin.site.register(Press)
+admin.site.register(Book)
+admin.site.register(Unit)
+admin.site.register(Lesson)
+admin.site.register(Word)
