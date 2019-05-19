25081fbb2bf6a1fb0da4566f50344b51bcff525a
Wang Xintian <wxintian@163.com>
2019-05-19 09:26:44 +0800

    static

--- happyproj/settings.py
+++ happyproj/settings.py
@@ -141,10 +141,10 @@ USE_TZ = False  #True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # STATIC_ROOT='/data/web/happyproj/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'commstatic')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'commstatic'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
